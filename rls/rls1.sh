1321ba9d96e6002104cab3701d6492a7f2ad3c02
Alex <130932460+Famelbyby@users.noreply.github.com>
2026-05-12 12:11:51 +0300

    Update rls1.sh

--- rls/rls1.sh
+++ rls/rls1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# ВКО Simulation — RLS-1: Воронеж-ДМ, Новосибирск
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 source "$PROJECT_DIR/config.sh"
