22985881ec0ec4d6ce60cdc5acdd0012eb09483f
Alex <130932460+Famelbyby@users.noreply.github.com>
2026-05-12 12:09:56 +0300

    Update zrdn1.sh

--- zrdn/zrdn1.sh
+++ zrdn/zrdn1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# ВКО Simulation — ZRDN-1: Томск, r=350 км
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 source "$PROJECT_DIR/config.sh"
